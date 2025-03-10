4b5a0af785576836c41ceffeddce79ebb43feae0
Suryansh <suryanshsinghshakya1@gmail.com>
2025-03-10 01:40:04 +0000

    add: constants

---
+++ app/constants.py
@@ -0,0 +1,22 @@
+TTS_VOICES = [
+    'am_adam',
+    'am_onyx',
+    'am_echo',
+    'am_eric',
+    'am_fenrir',
+    'am_liam',
+    'am_michael',
+    'am_puck',
+    'am_santa',
+    'af_heart',
+    'af_alloy',
+    'af_aoede',
+    'af_bella',
+    'af_jessica',
+    'af_kore',
+    'af_nicole',
+    'af_nova',
+    'af_river',
+    'af_sarah',
+    'af_sky'
+]
